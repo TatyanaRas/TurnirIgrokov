4a982a1d98f4b41be40a4425899a1b4803b2bd3b
Tatyana <tatyanalipch@yandex.ru>
2024-06-26 14:21:28 +0300

    src/

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,15 +1,27 @@
 //import jdk.internal.jimage.ImageStrings;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Game {
-    ArrayList<Player> players = new ArrayList<>();
+    ArrayList<Player> players = new ArrayList<>();//храним игроков в виде списков
     private Player player;
+  //  Map players= new HashMap();
+
+ //   HashMap<String, String> map = new HashMap<>();
+
+
+/* for (String name : map.player()) {
+     String value = map.get(name);
+    }*/
 
     //метод регистрации игрока
     public void register(Player player) {
-        players.add(player);
-    }
+       players.add(player);
+       // players.put(player.getName(), player);
+        }
+  //  }
 
     //метод соревнования
     public int round(String playerName1, String playerName2) {
@@ -24,13 +36,13 @@ public class Game {
                 player2 = player;
             }
 
-        }
+        }//если нет таких игроков
         if (player1 == null) {
             throw new NotRegisteredException(playerName1);
         }
         if (player2 == null) {
             throw new NotRegisteredException(playerName2);
-        }
+        }// сравниваем силу игроков
         if (player1.getStrength() > player2.getStrength()) {
             return 1;
 
